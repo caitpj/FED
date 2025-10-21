bfec43c1ba7a80826437f4e7d03666f05e121848
Cai Parry-Jones <caiparryjones96@gmail.com>
2025-10-21 12:23:44 +0100

    tidy up code

--- fed/data.py
+++ fed/data.py
@@ -5,6 +5,7 @@ my_data_path = 'fed/raw_data/GDP_data_in_current_USD.csv'
 def convert_csv_to_dataframe(file_path):
     return pd.read_csv(file_path)
 
-# Display the first 5 rows of the DataFrame
 df = convert_csv_to_dataframe(my_data_path)
+
+# Display the first 5 rows of the DataFrame - can delete later, just used for testing
 print(df.head())
