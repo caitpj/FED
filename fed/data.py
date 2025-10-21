c1f99312bd1985a773aa2ab17713ae25db667ef8
Cai Parry-Jones <caiparryjones96@gmail.com>
2025-10-21 12:24:43 +0100

    doc to function

--- fed/data.py
+++ fed/data.py
@@ -3,6 +3,7 @@ import pandas as pd
 my_data_path = 'fed/raw_data/GDP_data_in_current_USD.csv'
 
 def convert_csv_to_dataframe(file_path):
+    """Convert a CSV file to a pandas DataFrame."""
     return pd.read_csv(file_path)
 
 df = convert_csv_to_dataframe(my_data_path)
