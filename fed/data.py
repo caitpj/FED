70b211d26bd52e1aa359cd952bbeabb7500c983a
yfoxgit <yasminfox1998@gmail.com>
2025-10-22 09:46:40 +0100

    added function to convert data to long format, also checked for nulls

--- fed/data.py
+++ fed/data.py
@@ -10,3 +10,26 @@ df = convert_csv_to_dataframe(my_data_path)
 
 # Display the first 5 rows of the DataFrame - can delete later, just used for testing
 print(df.head())
+
+# convert to tidy format
+def wide_to_long(df):
+    ''' 
+    Use pandas melt to convert the dataframe from wide to long format
+    Assumes the data has Country Name in the left hand column, and then columns for each year showing GDP
+    '''
+    df = pd.melt(df, id_vars=['Country Name'], var_name = 'year', value_name = 'gdp')
+    return df
+
+df_long = wide_to_long(df)
+
+# Display the first 5 rows of the DataFrame - can delete later, just used for testing
+print(df_long.head())
+
+# handle nulls
+# since there are none in this data, could delete
+def isnull_any(df):
+    '''returns if there are any null values in each column'''
+    return df.isnull().any()
+print(isnull_any(df_long))
+
+
